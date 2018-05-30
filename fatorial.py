8b98bdf2a28c1136b5fe2e8ac342cced3383dde2
GustavoTelles <gustavotelles@me.com>
2018-05-29 23:43:11 -0300

    funcao com 0

--- fatorial.py
+++ fatorial.py
@@ -2,10 +2,13 @@
 Escreva um programa que receba um número natural n n na entrada e imprima n! n! (fatorial) na saída. usando while
 '''
 
-n = int(input('Digite o valor de n: '))
+n = int(input('Digite um numero: '))
 soma = 1
-while n != 1:
+while n != 1 and n != 0:
     soma = soma * n
     n = n - 1
 
-print(soma)
+if n == 0:
+    print (n + 1)
+else:
+    print(soma)
