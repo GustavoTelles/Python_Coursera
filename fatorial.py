b46fd82b0f257f1c792141f5448dc0fa868dc2fa
GustavoTelles <gustavotelles@me.com>
2018-05-29 22:41:25 -0300

    new version - 1 variavel

--- fatorial.py
+++ fatorial.py
@@ -2,11 +2,10 @@
 Escreva um programa que receba um número natural n n na entrada e imprima n! n! (fatorial) na saída. usando while
 '''
 
-n1 = n = int(input('Digite um numero: '))
-soma = 0
+n = int(input('Digite um numero: '))
+soma = 1
 while n != 1:
-    soma = n1 * (n - 1)
-    n1 = soma
+    soma = soma * n
     n = n - 1
 
 print('O valor fatorial é: ', soma)
