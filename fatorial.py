9fcba40cfed91a794a1444c2a06ead2d880edc9b
GustavoTelles <gustavotelles@me.com>
2018-05-29 23:08:17 -0300

    new somaNumeros.py

--- fatorial.py
+++ fatorial.py
@@ -2,10 +2,10 @@
 Escreva um programa que receba um número natural n n na entrada e imprima n! n! (fatorial) na saída. usando while
 '''
 
-n = int(input('Digite um numero: '))
+n = int(input('Digite o valor de n: '))
 soma = 1
 while n != 1:
     soma = soma * n
     n = n - 1
 
-print('O valor fatorial é: ', soma)
+print(soma)
--- numerosImpares.py
+++ numerosImpares.py
@@ -3,7 +3,7 @@ Receba um número inteiro positivo na entrada e imprima os n
 primeiros números ímpares naturais. Para a saída, siga o formato do exemplo abaixo.
 '''
 
-n = int(input('Digite a quantidade de números impares: '))
+n = int(input('Digite o valor de n: '))
 impar = 1
 while n != 0:
     print (impar)
